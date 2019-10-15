135d9ec11033deaaaa1273ae50d1570cc02a7aad
Ercan Erdoğan <ercanerdogan@gmail.com>
2019-10-15 13:20:27 +0300

    usafe of pointer

---
+++ ConsolePointer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsolePointer
+{
+    class Program
+    {
+        public static void Method()
+        {
+            unsafe
+            {
+                int x = 10;
+                int y = 20;
+                int* ptr1 = &x;
+                int* ptr2 = &y;
+                Console.WriteLine((int)ptr1);
+                Console.WriteLine((int)ptr2);
+                Console.WriteLine(*ptr1);
+                Console.WriteLine(*ptr2);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Method();
+            Console.ReadKey();
+
+
+        }
+    }
+}
